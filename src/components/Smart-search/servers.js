948c801cc9c4f8067644ae53f07b2e3a1d843e67
Fumi Hin <fumihin@outlook.com>
2020-09-13 20:47:54 +0300

    Small file for server list

---
+++ src/components/Smart-search/servers.js
@@ -0,0 +1,12 @@
+export const servers = [
+	{name: "Brasilia", id: "BR1"},
+	{name: "EUN1", id: "EUN1"},
+	{name: "Europe west", id: "EUW1"},
+	{name: "Japan", id: "JP1"},
+	{name: "Korea", id: "KR"},
+	{name: "LA1", id: "LA1"},
+	{name: "LA2", id: "LA2"},
+	{name: "Oceania", id: "OC1"},
+	{name: "Turkey", id: "TR1"},
+	{name: "Russia", id: "RU"}
+]
