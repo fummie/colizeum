63082b617d445e238a52c179765f9544452a74af
Fumi Hin <fumihin@outlook.com>
2021-02-25 15:44:13 +0300

    Rewrote server to have single handler
    Instead of multiple handler, now single handler decode json from front request and generate needed Riot api reqest.

--- server/server.go
+++ server/server.go
@@ -5,22 +5,43 @@ import (
 	"net/http"
 	"io/ioutil"
 	"github.com/rs/cors"
+	"encoding/json"
 )
 
-func getSummonerByName(w http.ResponseWriter, r *http.Request) {
-	//summoner, err := http.Get("/lol/summoner/v4/summoners/by-name/{summonerName}")
-	summoner, err := ioutil.ReadFile("./data/summoner.json")
+type body struct {
+	Operation string
+	Data      string
+}
+
+func fetchRiotAPI(writer http.ResponseWriter, request *http.Request) {
+	decoder := json.NewDecoder(request.Body)
+	var requestBody body
+	err := decoder.Decode(&requestBody)
+	if err != nil {
+		log.Println(err)
+	}
+
+	/*response, err := http.Get(requestBody.Operation + requestBody.Data)
 	if err != nil {
 		log.Fatalln(err)
+	}*/
+
+	var response []byte
+	switch requestBody.Operation {
+	case "/lol/summoner/v4/summoners/by-name/":
+		response, _ = ioutil.ReadFile("./data/summoner.json")
+	case "/lol/clash/v1/players/by-summoner/":
+		response, _ = ioutil.ReadFile("./data/teamMember.json")
 	}
-	w.Header().Set("Content-Type", "text/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write(summoner)
+
+	writer.Header().Set("Content-Type", "text/json; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(response)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/api/getSummonerByName", getSummonerByName)
+	mux.HandleFunc("/", fetchRiotAPI)
 	handler := cors.Default().Handler(mux)
 	err := http.ListenAndServe("localhost:8000", handler)
 	if err != nil {
